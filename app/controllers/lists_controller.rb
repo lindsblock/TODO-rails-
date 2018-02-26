0cbbda441f86b96b788f640359307a7e78578bae
Lindsay Block <lindsay.block05@gmail.com>
2018-02-26 16:37:27 -0700

    adding edit pages

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,35 @@
 class ListsController < ApplicationController
+  #GET
   def index
     @lists = List.all
   end
 
+#GET
   def show
     @list = List.find(params[:id])
   end
 
+#GET
   def new
     @list = List.new
   end
 
+#GET
+def edit
+  @list = List.find(params[:id])
+end
+
+#put
+def update
+  @list = List.find(params[:id])
+  if @list.update(lists_params)
+    redirect_to lists_path
+  else
+    render :edit
+  end
+end
+
+#POST
   def create
     @list = List.new(lists_params)
 
